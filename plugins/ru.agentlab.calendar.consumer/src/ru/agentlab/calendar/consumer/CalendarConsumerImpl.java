df42d5854dd5a3d28c8749c1e891b460fef65b89
Alex Ivanov <amivanoff@gmail.com>
2016-03-31 10:39:22 +0300

    Исправление копипаста

---
+++ plugins/ru.agentlab.calendar.consumer/src/ru/agentlab/calendar/consumer/CalendarConsumerImpl.java
@@ -0,0 +1,12 @@
+package ru.agentlab.calendar.consumer;
+
+import ru.agentlab.calendar.service.api.Event;
+import ru.agentlab.calendar.service.api.ICalendarServiceConsumer;
+
+public class CalendarConsumerImpl implements ICalendarServiceConsumer {
+
+	@Override
+	public void processEvent(Event e) {
+	}
+
+}
--- plugins/ru.agentlab.calendar.consumer/src/ru/agentlab/calendar/service/api/ICalendarServiceConsumer.java
+++
@@ -1,5 +0,0 @@
-package ru.agentlab.calendar.service.api;
-
-public interface ICalendarServiceConsumer {
-	public void processEvent(Event e);
-}
