c3079ee6a3e3d597283dab0b395348b195c71b58
Alex Ivanov <amivanoff@gmail.com>
2016-04-05 01:44:14 +0300

    Исправлены maven-зависимости Google Calendar

--- plugins/ru.agentlab.calendar.service.google/src/ru/agentlab/calendar/service/google/GoogleServiceImpl.java
+++ plugins/ru.agentlab.calendar.service.google/src/ru/agentlab/calendar/service/google/GoogleServiceImpl.java
@@ -1,9 +1,15 @@
 package ru.agentlab.calendar.service.google;
 
+import com.google.api.client.http.HttpTransport;
+
 import ru.agentlab.calendar.service.api.Event;
 import ru.agentlab.calendar.service.api.ICalendarService;
 
 public class GoogleServiceImpl implements ICalendarService{
+	
+	/** Global instance of the HTTP transport. */
+	private static HttpTransport httpTransport;
+	  
 	@Override
 	public void addEvent(Event e) {
 	}
